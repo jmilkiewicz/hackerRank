3dc60c49ca45ff34a414536da7eb467fc62b9bcd
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-09-01 14:20:03 +0200

    feat: Add StaircaseTest for testing the staircase function

---
+++ src/test/kotlin/StaircaseTest.kt
@@ -0,0 +1,43 @@
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class StaircaseTest {
+
+    @ParameterizedTest(name = "{index} for {0} biggest is {1}")
+    @MethodSource("provideStringsForIsBlank")
+    fun test(depth: Int, expected: List<String>) {
+        assertThat(app(depth), Matchers.`is`(expected))
+    }
+
+    fun staircase(n: Int): Unit {
+        app(n).forEach { println(it) }
+    }
+
+    fun app(n: Int): List<String> = (1..n)
+        .map {
+            val howManySpaces = (n - it)
+            " ".repeat(howManySpaces) + "#".repeat(n-howManySpaces)
+        }
+
+
+    companion object {
+        @JvmStatic
+        fun provideStringsForIsBlank(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    6,
+                    listOf("     #", "    ##", "   ###", "  ####", " #####", "######"),
+                ),
+                Arguments.of(
+                    1,
+                    listOf("#"),
+                ),
+            );
+        }
+    }
+
+}
